7dacccdf6f6e496f49dbb3879dd99686cbe5ff1b
ynero <ynero@softserveinc.com>
2021-08-13 13:58:06 +0300

    Logging message

--- src/main/java/com/example/DeviceService/services/categorizer/DeviceDataCategorizer.java
+++ src/main/java/com/example/DeviceService/services/categorizer/DeviceDataCategorizer.java
@@ -1,2 +1,13 @@
-package com.example.DeviceService.services.categorizer;public class DeviceDataCategorizer {
+package com.example.DeviceService.services.categorizer;
+
+import com.google.pubsub.v1.PubsubMessage;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class DeviceDataCategorizer {
+    public void messageHandling(PubsubMessage pubsubMessage){
+        log.info("message: {}",pubsubMessage);
+    }
 }
