4c24b3232ed0c6ff42514bcbc670ab7788646070
ynero <ynero@softserveinc.com>
2021-08-19 12:49:00 +0300

    SS-49: Added builders

--- src/main/java/com/ynero/ss/device_service/domain/Device.java
+++ src/main/java/com/ynero/ss/device_service/domain/Device.java
@@ -1,6 +1,7 @@
 package com.ynero.ss.device_service.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Document(collection = Device.COLLECTION_NAME)
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Device {
     public static final String COLLECTION_NAME = "devices";
     @Indexed(unique = true)
--- src/main/java/com/ynero/ss/device_service/domain/Port.java
+++ src/main/java/com/ynero/ss/device_service/domain/Port.java
@@ -1,6 +1,7 @@
 package com.ynero.ss.device_service.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Port {
 
     private String name;
