b084b0805561e17b39c1ae84feee5fb2160d35e3
ynero <ynero@softserveinc.com>
2021-08-19 16:29:20 +0300

    SS-49: Updated domain layer

--- src/main/java/com/ynero/ss/device_service/domain/Port.java
+++ src/main/java/com/ynero/ss/device_service/domain/Port.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -15,5 +16,6 @@ public class Port {
 
     private String name;
     private Object value;
+    private LocalDateTime lastUpdate;
     private UUID[] pipelinesId;
 }
