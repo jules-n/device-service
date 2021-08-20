7dfff044975aaecad9e27665a776709822c3df2b
ynero <ynero@softserveinc.com>
2021-08-20 12:15:14 +0300

    SS-49: Added config class for device service

--- src/main/java/com/ynero/ss/device_service/config/DeviceServiceConfig.java
+++ src/main/java/com/ynero/ss/device_service/config/DeviceServiceConfig.java
@@ -1,2 +1,15 @@
-package com.ynero.ss.device_service.config;public class DeviceServiceConfig {
+package com.ynero.ss.device_service.config;
+
+import com.ynero.ss.device_service.persistence.DeviceMongoService;
+import com.ynero.ss.device_service.persistence.DeviceService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DeviceServiceConfig {
+
+    @Bean
+    public DeviceService getDeviceService(){
+        return new DeviceMongoService();
+    }
 }
