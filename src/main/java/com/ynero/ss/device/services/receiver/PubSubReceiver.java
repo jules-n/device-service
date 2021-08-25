17a6b55fa0ceb9f045112dd0a7ed77b75d8a82cb
ynero <ynero@softserveinc.com>
2021-08-25 18:12:48 +0300

    hotfix/Pub-Sub-Message-Receiving-Fix: Added loger by dburyak, refactored pub/sub, gRPC sender, added controller for adding pipelines

--- src/main/java/com/ynero/ss/device/services/receiver/PubSubReceiver.java
+++ src/main/java/com/ynero/ss/device/services/receiver/PubSubReceiver.java
@@ -1,48 +1,79 @@
 package com.ynero.ss.device.services.receiver;
 
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.pubsub.v1.ProjectSubscriptionName;
-import com.ynero.ss.device.services.categorizer.DeviceDataCategorizer;
+import com.google.pubsub.v1.PubsubMessage;
 import com.ynero.ss.device.services.adapters.PubSubMessageToDeviceAdapter;
+import com.ynero.ss.device.services.categorizer.DeviceDataCategorizer;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.concurrent.TimeoutException;
 
-@ConditionalOnProperty(name = "enablingPubSub")
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+
 @Configuration
+@Log4j2
 public class PubSubReceiver {
 
-    @Setter(onMethod_={@Value("${spring.cloud.stream.bindings.input.destination}")})
+    private Subscriber subscriber;
+
+    @Setter(onMethod_ = {@Autowired}, onParam_ = {@Value("${spring.cloud.stream.bindings.input.destination}")})
     private String subscription;
 
-    @Setter(onMethod_={@Value("${spring.cloud.gcp.project-id}")})
+    @Value("${spring.cloud.gcp.project-id}")
     private String projectId;
 
-    @Setter(onMethod_={@Autowired})
+    @Setter(onMethod_ = {@Autowired})
     private DeviceDataCategorizer deviceDataCategorizer;
 
-    @Setter(onMethod_={@Autowired})
+    @Setter(onMethod_ = {@Autowired})
     private PubSubMessageToDeviceAdapter pubSubMessageToDeviceAdapter;
 
-
     @PostConstruct
-    public void asyncSubscribing() {
+    public void startPubSubSubscriber() {
+        log.debug("starting pub-sub subscriber: sub={}", subscription);
         var subscriptionName = ProjectSubscriptionName.of(projectId, subscription);
 
-        MessageReceiver receiver = (pubsubMessage, consumer) -> {
-            var device = pubSubMessageToDeviceAdapter.adapt(pubsubMessage);
-            deviceDataCategorizer.categorize(device);
+        MessageReceiver receiver = (PubsubMessage message, AckReplyConsumer consumer) -> {
+            /*var device = pubSubMessageToDeviceAdapter.adapt(pubsubMessage);
+            deviceDataCategorizer.categorize(device);*/
+            System.err.println(message);
             consumer.ack();
         };
+        subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
+        try {
+            subscriber.startAsync().awaitRunning(30, SECONDS);
+            log.info("pub-sub subscriber started: sub={}", subscription);
+        } catch (TimeoutException timeoutException) {
+            //after 30s
+            log.error("failed to start pub-sub subscriber", timeoutException);
+            try {
+                subscriber.stopAsync().awaitTerminated(10, SECONDS);
+            } catch (TimeoutException e) {
+                log.error("failed to shutdown pub-sub subscriber", e);
+            }
+        }
+    }
 
-        var subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
-        subscriber.startAsync().awaitRunning();
-        subscriber.awaitTerminated();
+    @PreDestroy
+    public void shutDownPubSubThreadPull() {
+        if (subscriber != null) {
+            log.info("closing pub-sub subscriber");
+            try {
+                subscriber.stopAsync().awaitTerminated(30, SECONDS);
+            } catch (TimeoutException e) {
+                log.error("failed to shutdown pub-sub subscriber", e);
+            }
+        }
     }
 
 
