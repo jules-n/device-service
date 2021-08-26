03085d69129b9b50dded9f47900e8544f454ca23
ynero <ynero@softserveinc.com>
2021-08-26 14:18:04 +0300

    refactoring/SS-74: Simplified name of device field

--- src/main/java/com/ynero/ss/device/domain/IncomingDeviceData.java
+++ src/main/java/com/ynero/ss/device/domain/IncomingDeviceData.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class IncomingDeviceData {
-    private Device incomingDevice;
-    private Port activePortOnIncomingDevice;
+    private Device device;
+    private Port activePort;
 }
