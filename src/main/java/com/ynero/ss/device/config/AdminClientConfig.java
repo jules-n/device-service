1d501e3c731b6f510997c37464dde8b839dda695
ynero <ynero@softserveinc.com>
2021-10-04 15:17:34 +0300

    feature/SS-33: Configured kafka bootstrap server. WIP configuration of info actuator endpoint

--- src/main/java/com/ynero/ss/device/config/AdminClientConfig.java
+++ src/main/java/com/ynero/ss/device/config/AdminClientConfig.java
@@ -3,18 +3,31 @@ package com.ynero.ss.device.config;
 import lombok.Setter;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Supplier;
+
 @Configuration
 public class AdminClientConfig {
 
+    @Setter(onMethod_ = {@Value("${spring.kafka.producer.bootstrap-servers}")})
+    private String bootstrapServers;
+
     @Setter(onMethod_ = {@Autowired})
-    KafkaAdmin kafkaAdmin;
+    private KafkaAdmin kafkaAdmin;
 
     @Bean
     public AdminClient getAdminClient(){
-        return AdminClient.create(kafkaAdmin.getConfigurationProperties());
+        var properties = new HashMap<String, Object>();
+        properties.putAll(kafkaAdmin.getConfigurationProperties());
+        properties.put("bootstrap.servers", List.of(bootstrapServers));
+        return AdminClient.create(properties);
     }
 }
