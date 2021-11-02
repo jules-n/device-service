5da8565008fa35fe5766cb3f3efa39fcf1c315ff
ynero <ynero@softserveinc.com>
2021-11-02 15:06:11 +0200

    Changed version of libs

--- src/main/java/com/ynero/ss/device/config/DTOToJSONConverterConfig.java
+++ src/main/java/com/ynero/ss/device/config/DTOToJSONConverterConfig.java
@@ -1,5 +1,6 @@
 package com.ynero.ss.device.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import json_converters.DTOToMessageJSONConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class DTOToJSONConverterConfig {
     @Bean
     DTOToMessageJSONConverter getDTOToMessageJSONConverter(){
-        return new DTOToMessageJSONConverter();
+        return new DTOToMessageJSONConverter(new ObjectMapper());
     }
 }
